6f1987c7175faef15eb6e5b6f5025b3c801e757a
Nahida305 <nahidaafrinmasuma@gmail.com>
2025-03-22 21:24:01 +0600

    java done

---
+++ index.js
@@ -0,0 +1,26 @@
+function showInfoRing(){
+    var ringInfo = document.getElementById("displayRingInfo");
+
+    ringInfo.innerHTML = `<p class = "infoText"> Ring Price: 349Taka <br>
+    Colors: Gold, Silver, Rose Gold <br>
+    Material: Base Metal, Gold, Silver</p>`;
+
+}
+
+function showInfoEarring(){
+    var earringInfo = document.getElementById("displayEarRingInfo");
+
+    earringInfo.innerHTML = `<p class = "infoText"> Ear Ring Price: 399Taka <br>
+    Colors: Gold, Silver <br>
+    Material: Base Metal, Gold, Silver, Pearls</p>`;
+
+}
+
+function showInfoNecklace(){
+    var necklaceInfo = document.getElementById("displayNecklaceInfo");
+
+    necklaceInfo.innerHTML = `<p class = "infoText"> Necklace Price: 999Taka <br>
+    Colors: Gold, Silver, Rose Gold <br>
+    Material: Base Metal, Gold, Silver, Silver Stone</p>`;
+
+}
